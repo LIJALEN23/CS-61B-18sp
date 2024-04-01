008574a0a9bf14f922582e0b5f0ed66c59458fa8
lijalen <14015606+lijalen@user.noreply.gitee.com>
2024-04-01 14:04:32 +0800

    增加了AList

---
+++ lecture/lecture6/lecture6/AList.java
@@ -0,0 +1,25 @@
+public class AList {
+    int[] items;
+    int size;
+
+    public AList() {
+        items = new int[100];
+        size = 0;
+    }
+
+    public void addList(int x) {
+
+    }
+
+    public int getLast() {
+
+    }
+
+    public int get(int i) {
+
+    }
+
+    public int size() {
+
+    }
+}
