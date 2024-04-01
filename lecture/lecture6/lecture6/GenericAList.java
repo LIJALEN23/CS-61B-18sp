e3f5818b3f481782ad3ef92833daea8c34b7aca5
lijalen <14015606+lijalen@user.noreply.gitee.com>
2024-04-01 15:06:27 +0800

    增加了AList和GenericAList

---
+++ lecture/lecture6/lecture6/GenericAList.java
@@ -0,0 +1,40 @@
+public class GenericAList<Item> {
+    private Item[] items;
+    private int size;
+
+    public GenericAList() {
+        items = (Item[]) new Object[100];
+        size = 0;
+    }
+    private void resize(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, temp, 0, size);
+        items = temp;
+    }
+
+    public void addList(Item x) {
+        if (items.length == size){
+            resize(size * 4);
+        }
+        items[size] = x;
+        size += 1;
+    }
+
+    public Item getLast() {
+        return items[size - 1];
+    }
+
+    public Item get(int i) {
+        return items[i];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public Item removeLast() {
+        Item x = items[size - 1];
+        size -= 1;
+        return x;
+    }
+}
---
+++ lecture/lecture6/lecture6/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args){
+        GenericAList<Integer> list = new GenericAList<>() ;
+        list.addList(1010);
+        list.addList(2020);
+        System.out.println(list.getLast());
+    }
+}
